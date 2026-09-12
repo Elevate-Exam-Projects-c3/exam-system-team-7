2fd57254a0492c858a71cd1749bc3691a91d0ecf
kareman <krefat@diginationmea.com>
2026-09-12 21:32:56 +0300

    Add Orchestractor for Add Quiz

---
+++ Features/Quizzes/AdminCreateQuiz/Handlers/CreateQuizOrchestratorHandler.cs
@@ -0,0 +1,39 @@
+﻿using exam_system.Features.Diplomas.GetDiplomaDetail.Queries;
+using exam_system.Features.Quizzes.AdminCreateQuiz.Commands;
+using exam_system.Features.Quizzes.AdminCreateQuiz.Orchestrators;
+using exam_system.Features.Shared;
+using MediatR;
+
+namespace exam_system.Features.Quizzes.AdminCreateQuiz.Handlers {
+    public class CreateQuizOrchestratorHandler : IRequestHandler<CreateQuizOrchestrator, RequestResponse<bool>> {
+
+       private readonly IMediator mediator;
+
+        public CreateQuizOrchestratorHandler(IMediator mediator) {
+            this.mediator = mediator;
+
+        }
+        public async Task<RequestResponse<bool>> Handle(CreateQuizOrchestrator request, CancellationToken cancellationToken) {
+
+            var diploma =  await mediator.Send(new GetDiplomaByIdQuery(request.DiplomaId));
+
+            if (diploma == null)
+                 throw new KeyNotFoundException($"Diploma with ID {request.DiplomaId} not found.");
+
+           var result =  await mediator.Send(new CreateQuizCommand(
+                request.DiplomaId,
+                request.Title,
+                request.Instructions,
+                request.DurationMinutes,
+                request.PassScore,
+                request.MaxAttempts,
+                request.StartDate,
+                request.EndDate
+                ));
+            if (!result.Success) {
+                throw new InvalidOperationException(result.Message);
+            }
+            return RequestResponse<bool>.Ok(true, result.Message);
+        }
+    }
+}
---
+++ Features/Quizzes/AdminCreateQuiz/Orchestrators/CreateQuizOrchestrator.cs
@@ -0,0 +1,14 @@
+﻿using exam_system.Features.Shared;
+using MediatR;
+
+namespace exam_system.Features.Quizzes.AdminCreateQuiz.Orchestrators {
+    public record CreateQuizOrchestrator (Guid DiplomaId,
+        string Title,
+        string? Instructions,
+        int DurationMinutes,
+        int? PassScore,
+        int? MaxAttempts,
+        DateTime StartDate,
+        DateTime EndDate ) : IRequest<RequestResponse<bool>> {
+     }
+}
