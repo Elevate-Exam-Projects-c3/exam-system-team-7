6400a9b328c65ed58536b6cb680cbb769f7eddcf
ahmed <ahmed.ae33@gmail.com>
2026-09-19 16:32:03 +0300

    Students Admin DashBoard overallscore & passed status

---
+++ Features/Diplomas/GetStudentDashboard/Orchestrators/StudentsDashBoardOrchestrator.cs
@@ -0,0 +1,8 @@
+﻿using exam_system.Dtos;
+using MediatR;
+
+namespace exam_system.Features.Diplomas.GetStudentDashboard.Orchestrators
+{
+    public record StudentsDashBoardOrchestrator(Guid userId) : IRequest<StudentsDashboardDto>;
+    
+}
