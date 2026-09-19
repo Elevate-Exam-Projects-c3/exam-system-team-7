5300a7630664cb27d84eeeeea2c04b86e1689196
ahmed <ahmed.ae33@gmail.com>
2026-09-19 13:23:48 +0300

    Some modifications that has been requested on AdminDashboard like making the reutn as double instead of int and retrive the readonly data using AsNoTracking

--- Features/Analytics/GetAdminDashboard/Queries/GetTotalAttemptsQuery.cs
+++ Features/Analytics/GetAdminDashboard/Queries/GetTotalAttemptsQuery.cs
@@ -2,6 +2,6 @@
 
 namespace exam_system.Features.Analytics.GetAdminDashboard.Queries
 {
-    public record GetTotalAttemptsQuery : IRequest<int>;
+    public record GetTotalAttemptsQuery : IRequest<double>;
     
 }
